dd652c6a94c7a032150de24fe0ca298fa95bbed4
oumou20 <oumo.diallo@edu.umi.ac.ma>
2025-05-07 22:44:20 +0100

    ajout pour flutter

--- src/main/java/com/so/wallet/controller/TestController.java
+++ src/main/java/com/so/wallet/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.so.wallet.controller;
 
+import com.so.wallet.entities.Utilisateur;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/protected")
-    public String testProtected() {
+    public String testProtected(@AuthenticationPrincipal Utilisateur utilisateur) {
         return "Bienvenue, tu es bien authentifiée 🎉";
     }
 }
